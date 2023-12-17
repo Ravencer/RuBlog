4b2422a531a9491ef28fb9fc8c4fe4d978a85b4b
Ravencer <ravencer880@gmail.com>
2023-12-17 23:10:31 +0700

    Creating routes on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-
+import {Layout} from './components/Layout.jsx'
+import {Routes, Route} from 'react-router-dom'
+import {MainPage} from './pages/MainPage.jsx'
+import {PostsPage} from './pages/PostsPage.jsx'
+import {Post} from './pages/Post.jsx'
+import {CreatePost} from './pages/CreatePost.jsx'
 
 function App() {
   return (
-    <div className="App"> </div>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<MainPage/>} />
+        <Route path='posts' element={<PostsPage/>} />
+        <Route path=':id' element={<Post/>} />
+        <Route path='new' element={<CreatePost/>} />
+      </Routes>
+    </Layout>
   );
 }
 
